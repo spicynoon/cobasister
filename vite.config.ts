91c9f2c4d667735fe24694f92f07e7f57db27599
zuhadharkasy <zuhadharkasy14@gmail.com>
2024-07-06 10:00:00 +0700

    implementing auth backend

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://equilibrium-backend-lqag4dnu5a-et.a.run.app',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
